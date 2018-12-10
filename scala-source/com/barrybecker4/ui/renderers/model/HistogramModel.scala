4065b1815fc36b4e82987e74040a77e92cccfc67
barrybecker4 <barrybecker4@yahoo.com>
2018-12-10 07:03:39 -0800

    Add more histogram model tests and make more accurate.

--- scala-source/com/barrybecker4/ui/renderers/model/HistogramModel.scala
+++ scala-source/com/barrybecker4/ui/renderers/model/HistogramModel.scala
@@ -1,11 +1,15 @@
-/*
- * Copyright by Barry G. Becker, 2018. Licensed under MIT License: http://www.opensource.org/licenses/MIT
- */
-
+// Copyright by Barry G. Becker, 2018. Licensed under MIT License: http://www.opensource.org/licenses/MIT
 package com.barrybecker4.ui.renderers.model
 
 import com.barrybecker4.common.math.function.InvertibleFunction
+import com.barrybecker4.common.math.Range
 
+/**
+  * Mainages the statistics for a histogram.
+  * The histogram this represent can be incrementally modified.
+  * @param data the initial histogram data
+  * @param xFunction manner in which the x domain is mapped. For example it can be linear or log scale.
+  */
 class HistogramModel(val data: Array[Int], val xFunction: InvertibleFunction) {
 
   assert(!data.exists(_ < 0), "Histograms can only have non-negative values")
@@ -17,22 +21,30 @@ class HistogramModel(val data: Array[Int], val xFunction: InvertibleFunction) {
   def getPositionForValue(value: Double): Int = xFunction.getValue(value).toInt
   def getMaxHeight = Math.max(1, data.max)
 
+  // Used only by unit test
+  def calcMedian: Double = {
+    val medianPos = calcMedianPos
+    val rightVal = if (medianPos >= numBars) xFunction.getDomain.max else getValueForPosition(medianPos + 1)
+    (getValueForPosition(medianPos) + rightVal) / 2.0
+  }
+
   def increment(xValue: Double): Unit = {
     val xPos = xFunction.getValue(xValue).toInt
     // it has to be in range to be shown in the histogram.
     if (xPos >= 0 && xPos < data.length) data(xPos) += 1
-    mean = (mean * sum + xValue) / (sum + 1)
+    mean = (sum * mean + xValue) / (sum + 1)
     sum += 1
   }
 
   def calcMean(): Double = {
     var sumXValues: Double = 0
     for (i <- data.indices) {
-      val binAvgX = xFunction.getInverseValue(i)
-        //(xFunction.getInverseValue(i) + xFunction.getInverseValue(i + 1)) / 2.0
+      // the bin holds all values between xInverse(i) and xInverse(i + 1)
+      val binAvgX = (xFunction.getInverseValue(i) + xFunction.getInverseValue(i + 1)) / 2.0
+        //xFunction.getInverseValue(i)
       sumXValues += data(i) * binAvgX
     }
-    sumXValues / sum
+    if (sum == 0) 0 else sumXValues / sum
   }
 
   def calcMedianPos: Int = {
--- scala-test/com/barrybecker4/ui/renderers/model/HistogramModelSuite.scala
+++ scala-test/com/barrybecker4/ui/renderers/model/HistogramModelSuite.scala
@@ -2,7 +2,7 @@
 package com.barrybecker4.ui.renderers.model
 
 import com.barrybecker4.common.math.Range
-import com.barrybecker4.common.math.function.LinearFunction
+import com.barrybecker4.common.math.function.{LinearFunction, LogFunction}
 import org.scalatest.FunSuite
 
 
@@ -11,70 +11,125 @@ class HistogramModelSuite extends FunSuite {
   test("model construction 2 bins (1, 2) low median" ) {
     val data = Array(2, 1)
     val model = new HistogramModel(data, new LinearFunction(Range(1, 2), data.length))
-    verifyResult(model, 1.1666666666666667, 3, 0, 1.0)
+    verifyResult(model, 1.4166666666666667,3.0,0,1.25)
   }
 
   test("model construction 2 bins (2, 3) low median" ) {
     val data = Array(2, 1)
     val model = new HistogramModel(data, new LinearFunction(Range(2, 3), data.length))
-    verifyResult(model, 2.1666666666666665, 3, 0, 2.0)
+    verifyResult(model, 2.4166666666666665,3.0,0,2.25)
   }
 
   test("model construction 2 bins (1, 3) low median" ) {
     val data = Array(2, 1)
     val model = new HistogramModel(data, new LinearFunction(Range(1, 3), data.length))
-    verifyResult(model, 1.3333333333333333, 3, 0, 1.0)
+    verifyResult(model, 1.8333333333333333,3.0,0,1.5)
   }
 
   test("model construction 2 bins (1, 2) high median" ) {
     val data = Array(1, 2)
     val model = new HistogramModel(data, new LinearFunction(Range(1, 2), data.length))
-    verifyResult(model, 1.3333333333333333, 3, 1, 1.5)
+    verifyResult(model, 1.5833333333333333,3.0,1,1.75)
   }
 
   test("model construction 2 bins (1, 3) high median" ) {
     val data = Array(1, 2)
     val model = new HistogramModel(data, new LinearFunction(Range(1, 3), data.length))
-    verifyResult(model, 1.6666666666666667, 3, 1, 2.0)
+    verifyResult(model, 2.1666666666666665, 3, 1, 2.5)
   }
 
   test("model construction 2 bins (1, 3) very high median" ) {
     val data = Array(1, 3)
     val model = new HistogramModel(data, new LinearFunction(Range(1, 3), data.length))
-    verifyResult(model, 1.75, 4, 1, 2.0)
+    verifyResult(model, 2.25, 4, 1, 2.5)
   }
 
    test("model construction with 3 bins") {
     val data = Array(2, 3, 5)
     val model = new HistogramModel(data, new LinearFunction(Range(0, 12), data.length))
-    verifyResult(model, 5.2, 10, 1, 4)
+    verifyResult(model, 7.2,10.0,1,6.0)
   }
 
   test("model construction 3 bins (1, 3)" ) {
     val data = Array(1, 4, 2)
     val model = new HistogramModel(data, new LinearFunction(Range(1, 4), data.length))
-    verifyResult(model, 2.142857142857143 /*15.0/7*/, 7, 1, 2.0)
+    verifyResult(model, 2.642857142857143,7.0,1,2.5)
   }
 
   test("model construction 4 bins (1, 4)" ) {
     val data = Array(2, 4, 2, 3)
     val model = new HistogramModel(data, new LinearFunction(Range(1, 5), data.length))
-    verifyResult(model, 2.5454545454545454 /*28.0/11.0*/, 11, 1, 2.0)
-    //  28/11 = n2.5454545454
+    verifyResult(model, 3.0454545454545454,11.0,1,2.5)
   }
 
   test("model construction 4 bins (0, 100)" ) {
     val data = Array(1, 2, 2, 3)  // 0, 25, 50, 75
     val model = new HistogramModel(data, new LinearFunction(Range(0, 100), data.length))
-    verifyResult(model, 46.875, 8, 2, 50.0)
+    verifyResult(model,59.375,8.0,2,62.5)
   }
+
+  test("incrementing model when 2 bins (0, 100)" ) {
+    val data = Array(0, 0)
+    val model = new HistogramModel(data, new LinearFunction(Range(0, 100), data.length))
+    model.increment(48.0)
+    model.increment(95.0)
+    verifyResult(model, 71.5,2.0,0,25.0) // expMedian wrong
+  }
+
+  test("incrementing model when 3 bins (0, 4)" ) {
+    val data = Array(0, 0, 0)
+    val model = new HistogramModel(data, new LinearFunction(Range(0, 4), data.length))
+    model.increment(0.8)
+    model.increment(0.7)
+    model.increment(2.9)
+    verifyResult(model, 1.4666666666666668,3.0,0,0.6666666666666666)
+  }
+
+  test("incrementing model when 2 bins (0, 100) and some values already in data" ) {
+    val data = Array(2, 5)
+    val model = new HistogramModel(data, new LinearFunction(Range(0, 100), data.length))
+    verifyResult(model, 60.714285714285715,7.0,1,75.0)
+    model.increment(48.0)
+    model.increment(95.0)
+    verifyResult(model, 63.111111111111114,9.0,1,75.0)
+  }
+
+  test("incrementing model with point outside domain when 3 bins (0, 4)" ) {
+    val data = Array(0, 0, 0)
+    val model = new HistogramModel(data, new LinearFunction(Range(0, 4), data.length))
+    model.increment(0.8)
+    model.increment(0.7)
+    model.increment(100.7)
+    model.increment(101.7)
+    model.increment(2.9)
+    verifyResult(model, 41.36000000000001,5.0,2,3.333333333333333)
+  }
+
+  test("incrementing model when 2 bins (0, 100) and log function on x" ) {
+    val data = Array(0, 0)
+    val xLogScale = 200
+    val model = new HistogramModel(data, new LogFunction(xLogScale, 10.0, false))
+    model.increment(48.0)
+    model.increment(95.0)
+    verifyResult(model, 71.5,2.0,1,1.0174362232703262)
+  }
+
+  test("incrementing model when 100 bins (0, 101) and log function on x" ) {
+    val data = Array.fill(101)(0)
+    val xLogScale = 200
+    val model = new HistogramModel(data, new LogFunction(xLogScale, 10.0, false))
+    model.increment(48.0)
+    model.increment(95.0)
+    verifyResult(model, 71.5,2.0,100,3.1805863849298888)
+  }
+
+  private case class Result(expMean: Double, expSum: Double, expMedianPos: Int, expMedian: Double)
 
   private def verifyResult(model: HistogramModel,
                    expMean: Double, expSum: Double, expMedianPos: Int, expMedian: Double) = {
-    assertResult(expMean) {model.mean}
-    assertResult(expSum) {model.sum}
+    val expResult = Result(expMean, expSum, expMedianPos, expMedian)
     val medianPos = model.calcMedianPos
-    assertResult(expMedianPos) {medianPos}
-    assertResult(expMedian) {model.getValueForPosition(medianPos)}
+    val actResult = Result(model.mean, model.sum, medianPos, model.calcMedian)
+    assertResult(expResult) {actResult}
   }
 }
